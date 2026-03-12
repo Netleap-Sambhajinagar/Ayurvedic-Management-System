d26106c5afe818f9e7188b36b5a5678426b7e6f7
Govindraj Kotalwar <govindrajkotalwar038@gmail.com>
2026-03-12 12:10:05 +0530

    Update database password placeholder in backend.py

--- backend.py
+++ backend.py
@@ -9,7 +9,7 @@ import mysql.connector
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "password": "Starbuster@2033",
+    "password": "INSERT_YOUR_PASSWORD",
     "database": "ayurveda"
 }
 
